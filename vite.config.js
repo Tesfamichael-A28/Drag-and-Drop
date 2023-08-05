b64220ef6e8ed09a21474d6255cbe64f37b90da5
Tesfamichael.A <tesfa722@gmail.com>
2023-08-05 18:42:04 +0300

    Prod

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Drag-and-Drop/"
+  base: "/Drag-and-Drop/",
+
+  build: {
+    outDir: "dist"
+  }
 })
